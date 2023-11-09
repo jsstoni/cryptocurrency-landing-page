14414fdec05b9e05c9c6962732b606bda3029347
jsstoni <jsstoniha@gmail.com>
2023-11-09 16:46:49 -0300

    arrow-svg

--- components/EmailAddress.vue
+++ components/EmailAddress.vue
@@ -1,7 +1,27 @@
 <template>
   <div
-    class="h-[50px] w-[245px] rounded-md bg-gradient-to-b from-[#18C8FF] via-[#933FFE] p-[1px]"
+    class="flex items-center h-[50px] w-[245px] rounded-md bg-gradient-to-b from-[#18C8FF] via-[#933FFE] p-[1px]"
   >
-    <input type="text" placeholder="Email Address" class="text-white h-[45px] w-full p-3 bg-zinc-950 rounded-lg focus:outline-none" />
+    <input
+      type="text"
+      placeholder="Email Address"
+      class="text-white h-[45px] w-full p-3 bg-zinc-950 rounded-lg focus:outline-none mb-auto"
+    />
+    <div class="h-full -ml-8 flex items-center">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12.9091 7L18 12M18 12L12.9091 17M18 12L6 12"
+          stroke="#FFFFFF"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
+      </svg>
+    </div>
   </div>
 </template>
